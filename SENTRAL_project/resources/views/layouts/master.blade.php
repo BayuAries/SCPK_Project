ad73267bd64139a3c6c3bba92e1fefd9e573fd52
BayuAries <62095092+BayuAries@users.noreply.github.com>
2020-07-02 19:41:00 +0700

    dikit

--- SENTRAL_project/resources/views/layouts/master.blade.php
+++ SENTRAL_project/resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
   <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a class="navbar-brand" href="/">Posyandu</a>
-            <a class="navbar-brand" href="/kader/admin">Kader</a>
+            <!-- <a class="navbar-brand" href="/kader/admin">Kader</a> -->
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <a href="/login" class="btn btn-outline-success ml-auto">Login</a>
             <a href="/register" class="btn btn-outline-danger ml-2">Daftar</a>
